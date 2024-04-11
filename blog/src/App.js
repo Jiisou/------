bf825a05891012566b59064320c702167cb0c0bc
jisujang <wltnslagg@sm.ac.kr>
2024-04-11 11:29:52 +0900

    state 데이터를 수정하는 예제 실습

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,3 +1,5 @@
+/*eslint-disable*/
+
 import logo from './logo.svg';
 import './App.css';
 import React, {useState} from 'react';
@@ -6,28 +8,36 @@ function App() {
 
   let post = '삼각지 고기 맛집';
 
-  let [글제목, 글제목변경] = useState(['시험기간 간식 배부','저가 커피 맛집']);
+  let [글제목, 글제목변경] = useState(['스타벅스 할인 코드','기말고사 간식 배부','수험생 의자 추천']);
+  let [좋아요, 좋아요증가] = useState(0);
 
+  function 제목바꾸기() {
+    var newArray = [...글제목];
+    newArray[0] = '더베이크 할인 코드';
+    글제목변경(newArray);
+  }
+  
   return (
     <div className="App">
       <div className="black-nav"> 
         <h4 style={{color:'orange', fontSize:'16pt'}}> 코딩애플 리액트 개발 중</h4>  
         </div>
 
+        <button onClick={ 제목바꾸기 }>새로운 제목</button>
         <div className="list">
-          <h3>{ post } </h3>
-          <p>4월 10일 발행</p>
-          <hr></hr>
+          <h3>{ 글제목[0] } <span onClick={()=> { 좋아요증가(좋아요+1) } }> 😽 {좋아요} </span></h3>
+          <p style={{color:'gray',fontSize:'13pt',fontWeight:'700', paddingBottom:'20px'}}>4월 11일 발행</p>
+          <hr style={{height:'1.5px', backgroundColor:'#1F85DE'}}></hr>
         </div>
         <div className="list">
           <h3>{ 글제목[1] } </h3>
-          <p>4월 11일 발행</p>
-          <hr></hr>
+          <p  style={{color:'gray',fontSize:'13pt',fontWeight:'700', paddingBottom:'20px'}}>4월 11일 발행</p>
+          <hr style={{height:'1.5px', backgroundColor:'#1F85DE'}}></hr>
         </div>
         <div className="list">
-          <h3>{ 글제목[0] } </h3>
-          <p>4월 11일 발행</p>
-          <hr></hr>
+          <h3>{ 글제목[2]}</h3>
+          <p  style={{color:'gray',fontSize:'13pt',fontWeight:'700', paddingBottom:'20px'}}> 4월 10일 발행</p>
+          <hr style={{height:'1.5px', backgroundColor:'#1F85DE'}}></hr>
         </div>
     </div>
   );
